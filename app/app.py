1446ccb92dc3ecd038cccee2b2802415cf4b55aa
Oscar Mike <mike.claure@gmail.com>
2022-10-14 12:53:01 +0200

    Add doctrings

--- app/app.py
+++ app/app.py
@@ -1,18 +1,25 @@
+"""
+This is the entry point of the application app
+"""
+
 from modules.module_a.arithmetic_operations import sum_values, multiply_values
 from modules.module_b.identity_operations import identity
 
 def main():
+    """
+    This is the main method for the app
+    """
     print("Hello World!")
-    a = 3
-    b = 5
+    value_a = 3
+    value_b = 5
 
-    c = sum_values(a, b)
-    d = multiply_values(a, b)
-    e = identity(a)
+    value_c = sum_values(value_a, value_b)
+    value_d = multiply_values(value_a, value_b)
+    value_e = identity(value_a)
 
-    print(f"The sum of {a} and {b} is {c}")
-    print(f"The multiplication of {a} and {b} is {d}")
-    print(f"value {a} is the same as {e}")
+    print(f"The sum of {value_a} and {value_b} is {value_c}")
+    print(f"The multiplication of {value_a} and {value_b} is {value_d}")
+    print(f"value {value_a} is the same as {value_e}")
 
 if __name__ == "__main__":
     main()
--- app/modules/module_a/arithmetic_operations.py
+++ app/modules/module_a/arithmetic_operations.py
@@ -1,5 +1,29 @@
-def sum_values(a, b):
-    return a + b
+"""
+This is a module to perform arithmetic operations
+"""
 
-def multiply_values(a, b):
-    return a * b
+def sum_values(value_a: float, value_b: float):
+    """
+        A method to add two values. Returns the sum.
+
+    Args:
+        value_a (float): value 1
+        value_b (float): value 2
+
+    Returns:
+        float: the sum of both values.
+    """
+    return value_a + value_b
+
+def multiply_values(value_a: float, value_b: float):
+    """
+        A method to multiply two values. Returns the multiplication.
+
+    Args:
+        value_a (float): value 1
+        value_b (float): value 2
+
+    Returns:
+        float: the multiplication of both values.
+    """
+    return value_a * value_b
