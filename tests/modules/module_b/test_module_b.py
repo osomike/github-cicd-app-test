da7c6b486ea0de9a6ce1b96ee23d51322e4ee0c9
Oscar Mike <mike.claure@gmail.com>
2022-10-14 13:20:32 +0200

    Test failure test

--- tests/modules/module_b/test_module_b.py
+++ tests/modules/module_b/test_module_b.py
@@ -3,7 +3,7 @@ import pytest
 from app.modules.module_b.identity_operations import identity
 
 
-@pytest.mark.parametrize("a, b", [(10, 10), (2, 2), ("A", "A")])
+@pytest.mark.parametrize("a, b", [(10, 10), (1, 2), ("A", "A")])
 def test_identity(a, b):
     res = identity(a)
     assert res == b
