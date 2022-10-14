6e4b6f04b61ae87bc1ae702a2558c14ebabbe7a2
Oscar Mike <mike.claure@gmail.com>
2022-10-14 08:36:02 +0200

    Add submodule

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
+from modules.module_a import sum_values, multiply_values
+
 def main():
     print("Hello World!")
+    a = 3
+    b = 5
+
+    c = sum_values(a, b)
+    d = multiply_values(a, b)
+
+    print(f"The sum of {a} and {b} is {c}")
+    print(f"The multiplication of {a} and {b} is {d}")
 
 if __name__ == "__main__":
     main()
---
+++ app/modules/module_a.py
@@ -0,0 +1,5 @@
+def sum_values(a, b):
+    return a + b
+
+def multiply_values(a, b):
+    return a * b
